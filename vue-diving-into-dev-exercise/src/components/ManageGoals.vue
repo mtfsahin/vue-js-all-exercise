224bd79dc92b98a755db67092ce9bfcbba7ca4e3
mtfsahin <mustafa9889.ma@gmail.com>
2023-01-11 21:02:58 +0300

    custom alert

--- vue-diving-into-dev-exercise/src/components/ManageGoals.vue
+++ vue-diving-into-dev-exercise/src/components/ManageGoals.vue
@@ -1,6 +1,37 @@
 <template>
   <div>
     <h2>ManageGoals</h2>
-    <input type="text" />
+    <input type="text" ref="goal"/>
+    <button @click="setGoal"> Set Goal</button>
+    <error-alert v-if="inputIsValid">
+        <h2>Input is Invalid</h2>
+        <p>Please enter at least a few characters...</p>
+        <button @click="confirmError">Okay</button>
+    </error-alert>
   </div>
 </template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue'
+export default {
+    components: {
+        ErrorAlert
+    },
+    data() {
+        return {
+            inputIsValid: false
+        }
+    },
+    methods:{
+        setGoal(){
+            const enteredValue = this.$refs.goal.value;
+            if(enteredValue === ''){
+               this.inputIsValid = true
+            }
+        },
+        confirmError(){
+            this.inputIsValid = false;
+        }
+    }
+}
+</script>

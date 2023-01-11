ea1ee07a1dc06d0cc4f6e8f5c700c7d62a578c18
mtfsahin <mustafa9889.ma@gmail.com>
2023-01-11 21:15:12 +0300

    teleporting elements

--- vue-diving-into-dev-exercise/src/components/ManageGoals.vue
+++ vue-diving-into-dev-exercise/src/components/ManageGoals.vue
@@ -1,37 +1,39 @@
 <template>
   <div>
     <h2>ManageGoals</h2>
-    <input type="text" ref="goal"/>
-    <button @click="setGoal"> Set Goal</button>
-    <error-alert v-if="inputIsValid">
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <teleport to="body">
+      <error-alert v-if="inputIsValid">
         <h2>Input is Invalid</h2>
         <p>Please enter at least a few characters...</p>
         <button @click="confirmError">Okay</button>
-    </error-alert>
+      </error-alert>
+    </teleport>
   </div>
 </template>
 
 <script>
-import ErrorAlert from './ErrorAlert.vue'
+import ErrorAlert from "./ErrorAlert.vue";
 export default {
-    components: {
-        ErrorAlert
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsValid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") {
+        this.inputIsValid = true;
+      }
     },
-    data() {
-        return {
-            inputIsValid: false
-        }
+    confirmError() {
+      this.inputIsValid = false;
     },
-    methods:{
-        setGoal(){
-            const enteredValue = this.$refs.goal.value;
-            if(enteredValue === ''){
-               this.inputIsValid = true
-            }
-        },
-        confirmError(){
-            this.inputIsValid = false;
-        }
-    }
-}
+  },
+};
 </script>

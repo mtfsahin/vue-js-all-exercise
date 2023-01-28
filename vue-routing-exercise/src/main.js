39c3292686cf68b6349f2a030ad30a223c43c63e
mtfsahin <mustafa9889.ma@gmail.com>
2023-01-28 17:17:19 +0300

    vue router exercise

---
+++ vue-routing-exercise/src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHashHistory} from 'vue-router';
+
+import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue'
+import UsersList from './components/users/UsersList.vue'
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        {path: '/teams', component: TeamsList},
+        {path: '/users', component: UsersList}
+    ]
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');

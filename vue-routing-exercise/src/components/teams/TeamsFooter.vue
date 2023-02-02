26813a94c3c4af9bfeeb18ccfe8a541851b0418e
mtfsahin <mustafa9889.ma@gmail.com>
2023-02-02 23:48:45 +0300

    multiple routes with named router

---
+++ vue-routing-exercise/src/components/teams/TeamsFooter.vue
@@ -0,0 +1,5 @@
+<template>
+    <h2>
+        Temas Footer
+    </h2>
+</template>
--- vue-routing-exercise/src/main.js
+++ vue-routing-exercise/src/main.js
@@ -6,18 +6,23 @@ import TeamsList from './components/teams/TeamsList.vue'
 import UsersList from './components/users/UsersList.vue'
 import TeamMembers from './components/teams/TeamMembers.vue'
 import NotFound from './components/nav/NotFound.vue'
+import TeamsFooter from './components/teams/TeamsFooter.vue'
+import UsersFooter from './components/users/UsersFooter.vue'
 
 const router = createRouter({
     history: createWebHashHistory(),
+   
     routes: [
         { path: '/', redirect: '/teams' },
         //alias da kullanılabilir
         {
-            path: '/teams',name:'teams', component: TeamsList, children: [
+            path: '/teams',name:'teams',
+            components: {default: TeamsList, footer:TeamsFooter },
+            children: [
                 { path: '/teams/:teamId', name:'team-members', component: TeamMembers, props: true },
             ]
         },
-        { path: '/users', component: UsersList },
+        { path: '/users', components: {default: UsersList, footer:UsersFooter }},
         { path: '/:notFound(.*)', component: NotFound }
     ],
     linkActiveClass: 'active',

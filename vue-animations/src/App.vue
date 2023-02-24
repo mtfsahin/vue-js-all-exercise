89366be68a5d458f7935d0c31fc272a293eb2da7
Mustafa Şahin <63456077+mtfsahin@users.noreply.github.com>
2023-02-24 18:22:15 +0300

    css basic animations

--- vue-animations/src/App.vue
+++ vue-animations/src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="container">
-    <div class="block"></div>
-    <button>Animate</button>
+    <div class="block" :class="{animate: animatedBlock}"></div>
+    <button @click="animateBlock" >Animate</button>
   </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
-  </base-modal>
+  </base-modal> 
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
@@ -15,9 +15,15 @@
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false };
+    return { 
+      animatedBlock: false,
+      dialogIsVisible: false,
+     };
   },
   methods: {
+    animateBlock(){
+      this.animatedBlock = true;
+    },
     showDialog() {
       this.dialogIsVisible = true;
     },
@@ -57,6 +63,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
+  /*transition: transform 0.3s ease-out;*/
 }
 .container {
   max-width: 40rem;
@@ -69,4 +76,22 @@ button:active {
   border: 2px solid #ccc;
   border-radius: 12px;
 }
+.animate {
+  /*transform: translateX(-50px);*/
+  animation: slide-fade 0.3s forwards;
+}
+
+@keyframes slide-fade {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+  
+  70% {
+    transform: translateX(-120) scale(1.1);
+  }
+  100% {
+    transform: translateX(-150) scale(1);
+  }
+}
+
 </style>
